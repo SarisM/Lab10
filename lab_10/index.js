64d08ea5b98e905d5dddf93cf60c6541c918b0fa
sarism <saraisabelmayalasso@gmail.com>
2024-04-20 21:05:36 -0500

    s

--- lab_10/index.js
+++ lab_10/index.js
@@ -1,29 +1,31 @@
-fetch('https://api.escuelajs.co/api/v1/products')
-	.then(response => {
-		if (response.ok) {
-			return response.json()
-		}
-	})
-	.then(data => {
-		const $container = document.getElementById('box-galery')
-		data.forEach(({ title, images }) => {
-			const validateLink = images.filter(img => !img.startsWith('[') && !img.endsWith(']'))
-			if (validateLink.length > 0) {
-				const boxImage = document.createElement('div')
-				boxImage.classList.add('box-img')
-				const img = document.createElement('img')
-				img.classList.add('tipos-decafe')
-				img.src = validateLink
-				const a = document.createElement('a')
-				a.classList.add('btn-img')
-				a.href = '#'
-				a.textContent = title
-				boxImage.appendChild(img)
-				$container.appendChild(a)
-				$container.appendChild(boxImage)
-			}
-		})
-	})
-	.catch(error => {
-		throw new Error(error)
-	})
+fetch("https://api.escuelajs.co/api/v1/products")
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    const container = document.getElementById("box-galery");
+    data.forEach((item) => {
+      if (
+        item.images &&
+        item.images.length > 0 &&
+        typeof item.images[0] === "string" &&
+        item.images[0].startsWith("https")
+      ) {
+        const boxImage = document.createElement("div");
+        boxImage.classList.add("box-img");
+        const img = document.createElement("img");
+        img.classList.add("tipos-decafe");
+        img.src = item.images[0];
+        const a = document.createElement("a");
+        a.classList.add("btn-img");
+        a.href = "#";
+        a.textContent = item.title;
+        boxImage.appendChild(img);
+        container.appendChild(boxImage);
+        container.appendChild(a);
+      }
+    });
+  })
+  .catch((error) => {
+    throw new Error(error);
+  });
